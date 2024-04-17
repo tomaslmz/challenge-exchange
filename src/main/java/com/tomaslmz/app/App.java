47e3249d973be1442eaf38584408adad62436c87
Tomás <tomas153lm@gmail.com>
2024-04-17 08:42:40 -0300

    feat: add do while to project

--- src/main/java/com/tomaslmz/app/App.java
+++ src/main/java/com/tomaslmz/app/App.java
@@ -20,71 +20,77 @@ public class App {
 
       int from = -1;
 
-      System.out.println("Welcome to the Challenge Exchange!");
-
-      System.out.println("Type a number to start to use!");
-      System.out.println("""
-        1 - BRL
-        2 - USD
-        3 - EUR
-        4 - JPY
-        5 - ARS
-      """);
       do {
-        from = sc.nextInt();
+        System.out.println("Welcome to the Challenge Exchange!");
 
-        if(from < 0 || from > 5) {
-          System.out.println("Please, type a valid option!");
+        System.out.println("Type a number to start to use!");
+        System.out.println("""
+          1 - BRL
+          2 - USD
+          3 - EUR
+          4 - JPY
+          5 - ARS
+          0 - To exit the program
+        """);
+        do {
+          from = sc.nextInt();
+
+          if(from < 0 || from > 5) {
+            System.out.println("Please, type a valid option!");
+          }
+        } while(from < 0 || from > 5);
+
+        if(from != 0) {
+          System.out.println("Ok! The selected type was " + options[from] + ". Now, you should select another option!");
+
+          int to = -1;
+
+          System.out.println("""
+            1 - BRL
+            2 - USD
+            3 - EUR
+            4 - JPY
+            5 - ARS
+          """);
+
+          do {
+            to = sc.nextInt();
+
+            if(to < 0 || to > 5) {
+              System.out.println("Please, type a valid option!");
+            }
+
+            if(to == from) {
+              System.out.println("Please, select a different option!");
+            }
+          } while(to < 0 || to > 5 || to == from);
+
+        System.out.println("Ok! The conversion will be from " + options[from] + " to " + options[to] + "! Now, you should type an amount!");
+
+          double amount = -1;
+
+          do {
+            System.out.print("\nType your amount: ");
+            amount = sc.nextDouble();
+
+            if(amount <= 0) {
+              System.out.println("Please, type a valid amount!");
+            }
+          } while(amount <= 0);
+
+          Currency currency = new Currency(options[from], amount);
+
+          System.out.println(currency.getCurrency());
+
+          Exchange exchange = new Exchange();
+
+          System.out.println("\nCONVERTED");
+
+          Currency convertedCurrency = exchange.convert(amount, options[from], options[to]);
+
+          System.out.println(convertedCurrency.getCurrency());
         }
-      } while(from < 0 || from > 5);
-
-      System.out.println("Ok! The selected type was " + options[from] + ". Now, you should select another option!");
-
-      int to = -1;
-
-      System.out.println("""
-        1 - BRL
-        2 - USD
-        3 - EUR
-        4 - JPY
-        5 - ARS
-      """);
-
-      do {
-        to = sc.nextInt();
-
-        if(to < 0 || to > 5) {
-          System.out.println("Please, type a valid option!");
-        }
-
-        if(to == from) {
-          System.out.println("Please, select a different option!");
-        }
-      } while(to < 0 || to > 5 || to == from);
-
-      System.out.println("Ok! The conversion will be from " + options[from] + " to " + options[to] + "! Now, you should type an amount!");
-
-      double amount = -1;
-
-      do {
-        System.out.print("\nType your amount: ");
-        amount = sc.nextDouble();
-
-        if(amount <= 0) {
-          System.out.println("Please, type a valid amount!");
-        }
-      } while(amount <= 0);
-
-      Currency currency = new Currency(options[from], amount);
-
-      System.out.println(currency.getCurrency());
-
-      Exchange exchange = new Exchange();
-
-      Currency convertedCurrency = exchange.convert(amount, options[from], options[to]);
-
-      System.out.println(convertedCurrency.getCurrency());
-
+      } while(from != 0);
       sc.close();
     }
 }
